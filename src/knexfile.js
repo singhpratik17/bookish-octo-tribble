165d459ee6d04aa4e2342423f6cfd674e231cda2
Pratik Singh <pratiktsingh17@gmail.com>
2021-09-14 10:54:05 +0530

    - Added knex, postgres.

---
+++ src/knexfile.js
@@ -0,0 +1,19 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: "busy-panda",
+      user: "postgres",
+      password: "groot",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      database: "busy-panda",
+      user: "postgres",
+      password: "groot",
+    },
+  },
+};
